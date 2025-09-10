3d2f8719e44e6968177f0397ad99caba2096e9f4
Priyanshu Kumar <priyanshudkj@gmail.com>
2025-09-10 20:01:52 +0530

    AttendSmart

---
+++ firebase.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAiA01gp9DhzgyNNMiFNHj396yv_rCkgMc",
+
+  authDomain: "myawesomeproject-1f263.firebaseapp.com",
+
+  databaseURL: "https://myawesomeproject-1f263-default-rtdb.firebaseio.com", 
+
+  projectId: "myawesomeproject-1f263",
+
+  storageBucket: "myawesomeproject-1f263.firebasestorage.app",
+
+  messagingSenderId: "669778868857",
+
+  appId: "1:669778868857:web:6c761246a5769cebab32a8",
+  };
+  
+  // Initialize Firebase
+  // This line connects your app to your Firebase project using the keys above.
+  firebase.initializeApp(firebaseConfig);
